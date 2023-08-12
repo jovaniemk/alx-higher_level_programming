764f422d8f28de2e4b1ec732a07b4d4f1e71cf8b
jovaniemk <jovaniemk@gmail.com>
2023-08-12 11:27:56 +0000

    building a simple command line calculator

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
-#!/user/bin/python3
-
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    def add(a, b):
-        return a + b
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
