373cb347317f9e860804b052e8739b2dbb8849d4
jovaniemk <jovaniemk@gmail.com>
2024-04-23 02:02:42 -0700

    java script

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
-// script that prints 3 lines.
-
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
